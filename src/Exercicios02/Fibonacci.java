f75fdb4bf3dd6624f5564c1398a4b4bede7cb144
tsgarcia <thamires.luz@outlook.com>
2016-04-20 19:05:01 -0300

    Sequencia Fibonacci

--- src/Exercicios02/Fibonacci.java
+++ src/Exercicios02/Fibonacci.java
@@ -1,5 +1,34 @@
 package Exercicios02;
 
 public class Fibonacci {
+	private int n1;
+	private int n2;
+	
+	public Fibonacci() {
+		this.n1=0;
+		this.n2=1;
+	}
+	public int getN1() {
+		return n1;
+	}
+	public void setN1(int n1) {
+		this.n1 = n1;
+	}
+	public int getN2() {
+		return n2;
+	}
+	public void setN2(int n2) {
+		this.n2 = n2;
+	}
+
+	public void sequenceFibonacci(int v){
+		System.out.print(n1);
+		for (int i = 0; i < v; i++) {
+			System.out.print(" "+n2);
+			n2=n1+n2;
+			n1=n2-n1;
+		}
+	}
+	
 	
 }
--- src/Exercicios02/MainFibonacci.java
+++ src/Exercicios02/MainFibonacci.java
@@ -1,9 +1,15 @@
 package Exercicios02;
 
+import javax.swing.JOptionPane;
+
 public class MainFibonacci {
 
 	public static void main(String[] args) {
+		Fibonacci n = new Fibonacci();
 
+		n.sequenceFibonacci(Integer
+				.parseInt(JOptionPane.showInputDialog("Digite a quantidade "
+						+ "desejado para a sequencia Fibonacci: ")));
 	}
 
 }
