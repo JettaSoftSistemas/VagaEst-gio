c2ca126e4acc3010e21f158912fe7cc7ea805411
tsgarcia <thamires.luz@outlook.com>
2016-04-21 16:42:14 -0300

    Refresh

--- src/Exercicio02/Fibonacci.java
+++ src/Exercicio02/Fibonacci.java
@@ -1,4 +1,4 @@
-package Exercicio03;
+package Exercicio02;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -6,43 +6,41 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 
-public class CaixaEletronico {
-	private double saldo;
+public class Fibonacci {
+	private int n1;
+	private int n2;
 
-	public double getSaldo() {
-		return saldo;
+	// Contrutor inicializando os valores das variaveis.
+	public Fibonacci() {
+		this.n1 = 0;
+		this.n2 = 1;
 	}
 
-	public void setSaldo(double saldo) {
-
-		this.saldo = saldo;
-
+	public int getN1() {
+		return n1;
 	}
 
-	// Metodo saque
-	public void saque(double saque) {
-		if (saque > 0 || saque < saldo) {
-			saldo = saldo - saque;
-		}
+	public void setN1(int n1) {
+		this.n1 = n1;
 	}
 
-	// Metodo deposito
-	public void deposito(double deposito) {
-		if (deposito > 0) {
-			saldo = saldo + deposito;
-		} else {
-			System.out.println("Valor Invalido");
-		}
+	public int getN2() {
+		return n2;
 	}
 
-	// Extrato
-	public void imprimirDados() {
+	public void setN2(int n2) {
+		this.n2 = n2;
+	}
+
+	// Executando a sequencia Fibonacci com a quantidade escolhida pelo usuario.
+
+	public void sequenceFibonacci(int v) {
 		try {
 
 			// Criacao e o caminho em que o diretorio sera criado.
-			File diretorio = new File("Caixa");
+			File diretorio = new File("Fibonacci");
 			// Criacao e nome do arquivo que sera criado.
-			File arquivo = new File("CaixaEletronico.txt");
+			File arquivo = new File("Fibonacci.txt");
 
 			if (!arquivo.exists()) {
 				arquivo.createNewFile();
@@ -51,8 +49,12 @@ public class CaixaEletronico {
 			FileWriter fw = new FileWriter(arquivo);
 			BufferedWriter bw = new BufferedWriter(fw);
 			bw.newLine();
-			bw.write("Saldo: " +saldo + " R$");
-
+			for (int i = 0; i < v; i++) {
+				bw.write(n2+" ");
+				System.out.print(" " + n2);
+				n2 = n1 + n2;
+				n1 = n2 - n1;
+			}
 			// Onde sera incluido todos os metodos que quero que apareca no
 			// arquivo texto.
 			bw.close();
@@ -63,8 +65,6 @@ public class CaixaEletronico {
 
 		} catch (Exception e) {
 		}
-		System.out.println("Saldo: " + saldo + " R$");
-
+		
 	}
-
 }
